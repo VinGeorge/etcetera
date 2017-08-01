b6ae294f9a00a3d6b4dba06e73ce82021220c1bf
VinGeorge <Gorgevin@gmail.com>
2017-08-01 22:18:23 +0300

    Add files via upload

--- Netology_API_Class.py
+++ Netology_API_Class.py
@@ -4,29 +4,51 @@ from urllib.parse import urlencode
 import requests
 
 AUTHORIZE_URL = 'https://oauth.yandex.ru/authorize'
-APP_ID = '322df0d4993341be907ff3ca4bfb2b7a'
+APP_ID = 'a655eeac5d7c4b5b8c1f70ac42e43acd'
 
 auth_data = {
     'response_type': 'token',
     'client_id': APP_ID
 }
 
-print('?'.join((AUTHORIZE_URL, urlencode(auth_data))))
+# print('?'.join((AUTHORIZE_URL, urlencode(auth_data))))
 
-TOKEN = 'AQAAAAANIfuDAAR0dfPWw9ZBqEGnoiTSWQDgxLE'
-
-
-def get_counter():
-    url = 'https://api-metrica.yandex.ru/management/v1/counters'
-    headers = {
-        'Authorization': 'OAuth {}'.format(TOKEN),
-        'Content-Type': 'application/x-yametrika+json'
-    }
-    response = requests.get(url, headers=headers)
-    print(response.status_code)
-    pprint(response.json())
+# TOKEN = 'AQAAAAANIfuDAAR0dfPWw9ZBqEGnoiTSWQDgxLE'
 
 
-get_counter()
+class ApiYandexMetrika:
 
-# Здравствуйте, Артём. Спасибо за лекцию! Просьба вернуть работу на доработку
+    def __init__(self, TOKEN):
+        self.TOKEN = TOKEN
+
+    def get_headers(self):
+        return {
+            'Authorization': 'OAuth {}'.format(self.TOKEN),
+            'Content-Type': 'application/x-yametrika+json'
+        }
+
+    def get_counters(self):
+        url = 'https://api-metrica.yandex.ru/management/v1/counters'
+        headers = self.get_headers()
+        response = requests.get(url, headers=headers)
+        counters = response.json()['counters']
+        return counters
+
+    def get_counter_data(self, counter_id):
+        url = 'https://api-metrika.yandex.ru/stat/v1/data'
+        headers = self.get_headers()
+        params = {
+            'id': counter_id,
+            'metrics': 'ym:s:visits',
+            # 'views': "ym:s:pageviews",
+            # 'users': "ym:s:users"
+        }
+        response = requests.get(url, params, headers = headers)
+        return response.json()
+
+
+ym = ApiYandexMetrika('AQAAAAANIfuDAAR1CuoCLJJPYEkWiEPhL2bAoao')
+counters = ym.get_counters()
+ids = [c['id'] for c in counters]
+pprint(ym.get_counter_data(ids[0]))
+
